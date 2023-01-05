7083dbe139286a32d4451ceadd8339a855df5b62
j.montero.santiago <j.montero.santiago@accenture.com>
2023-01-05 15:35:40 -0600

    Added JWT authentication, removed previous login page redirection

--- src/main/java/com/mx/accenture/springmvc/ejemplo/controller/ControladorPrincipal.java
+++ src/main/java/com/mx/accenture/springmvc/ejemplo/controller/ControladorPrincipal.java
@@ -1,18 +1,20 @@
 package com.mx.accenture.springmvc.ejemplo.controller;
 
-import com.mx.accenture.springmvc.ejemplo.dao.CursosRepository;
 import com.mx.accenture.springmvc.ejemplo.model.Cursos;
 import com.mx.accenture.springmvc.ejemplo.service.ICursosService;
 import java.util.ArrayList;
 import java.util.List;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
 @Slf4j
+@RestController
+@RequestMapping("/")
 public class ControladorPrincipal{
 
     @Autowired
@@ -22,7 +24,7 @@ public class ControladorPrincipal{
     public String presentacion(Model model) {
         log.info("Ejecutando controlador Spring MVC");
         var mensajeSalida = "Mensaje desde el controlador";
-        
+
         Cursos curso1 = new Cursos();
         curso1.setIdCurso(1);
         curso1.setNombreCurso("Fundamentos de Spring MVC");
@@ -30,7 +32,7 @@ public class ControladorPrincipal{
         curso1.setNombreProfesor("Kevin");
         curso1.setNumeroAlumnos(100);
         curso1.setNumeroLecciones(1);
-        
+
         Cursos curso2 = new Cursos();
         curso2.setIdCurso(2);
         curso2.setNombreCurso("Uso de Spring MVC");
@@ -38,17 +40,15 @@ public class ControladorPrincipal{
         curso2.setNombreProfesor("Kevin");
         curso2.setNumeroAlumnos(50);
         curso2.setNumeroLecciones(5);
-        
+
         List<Cursos> cursos1 = new ArrayList();
-        
+
         List<Cursos> cursos = new ArrayList();
         cursos.add(curso1);
         cursos.add(curso2);
-        
-        model.addAttribute("mensajeController", mensajeSalida);
-      //model.addAttribute("objetocurso1", curso1);
-        model.addAttribute("listaPersonas", cursos);
-        return "index";
+
+
+        return mensajeSalida;
     }
 
     @GetMapping("/listar")
